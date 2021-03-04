e36c93a56a0d0afec4faf4bda4704d595e560313
Sakurai07 <cool5tarXV@gmail.com>
2021-03-04 10:46:51 +0800

    added electron. user friendly ui and less steps

---
+++ install-dep.sh
@@ -0,0 +1,16 @@
+npm install
+npm i --save-dev electron
+
+echo "If you don't have mongo DB you can get it from here, https://mongodb.com"
+
+mongo < mongoSetup
+
+echo "config.json"
+echo "go to config.json"
+echo "check https://www.youtube.com/watch?v=VqML5F8hcRQ for help"
+
+gulp
+npm start
+
+
+echo "run start.exe (win) or start.sh ()linux/mac"
---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  console.log("remember to check the config files")
+  console.log("or check docs.txt for the steps")
+  win.loadFile('http://localhost:3000')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
